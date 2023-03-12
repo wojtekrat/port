c911d81fee92a54005dbea4410a4e66653378d38
wojtekrat <wojciech.ratajczak92@gmail.com>
2023-03-12 20:40:40 +0100

    new

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import Blob from '../components/Blob'
+
+const Hero = ({ text, text1 }) => {
+  return (
+    <div className="absolute top-0 left-0 bg-gradient-to-r from-zinc-900 via-stone-900 to-green-900 text-white w-full h-full ">
+      <div className='flex flex-col md:flex-row justify-center items-center mt-[200px] md:mt-[120px]'>
+        <div>
+          <div>
+            <h1 className='text-[40px] lg:text-[60px] text-white typed-out'>
+              {text}
+            </h1>
+          </div>
+          <div>
+            <h1 className='text-[30px] lg:text-[40px] text-white typed-out2'>
+              {text1}
+            </h1>
+          </div>
+        </div>
+        <div className='w-[400px] h-[400px] md:w-[600px] md:h-[600px] ml-[50px]'>
+          <Canvas  camera={{ position: [0.0, 0.0, 8.0]}}>
+            <Blob/>
+          </Canvas>
+        </div>
+      </div>   
+    </div>
+
+
+  )
+}
+
+export default Hero
---
+++ src/components/hero/fragmentShader.js
@@ -0,0 +1,17 @@
+const fragmentShader = `
+uniform float u_intensity;
+uniform float u_time;
+varying vec2 vUv;
+varying float vDisplacement;
+void main() {
+    float distort = 2.0 * vDisplacement * u_intensity * sin(vUv.y * 10.0 + u_time);
+    vec3 color = mix(
+        mix(vec3(23.0, 23.0, 23.0) / 255.0, vec3(20.0, 83.0, 45.0) / 255.0, abs(vUv.y - 0.2) * 5.0),
+        mix(vec3(22.0, 101.0, 52.0) / 255.0, vec3(22.0, 163.0, 74.0) / 255.0, abs(vUv.y - 0.5) * 5.0),
+        abs(vUv.y - 0.8) * 5.0
+    );
+    color = mix(color, vec3(0.0), abs(vUv.x - 0.5) * 2.0); // add black stripes
+    gl_FragColor = vec4(color, 1.0);
+}
+`;
+export default fragmentShader;

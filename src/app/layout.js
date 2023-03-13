b446bc43515e1a0d2dbd7c2b95b0a6aad2473e7e
wojtekrat <wojciech.ratajczak92@gmail.com>
2023-03-13 23:41:31 +0100

    newnewnew

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className="bg-gradient-to-r from-zinc-900 via-stone-900 to-zinc-900 text-white">
         <head>
           <title>Wojciech Ratajczak</title>
+          <link rel="icon" type="image/png" sizes="32x32" href="/logo.png"></link>
         </head>
         <body>
           <header>

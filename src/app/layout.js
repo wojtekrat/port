1896166b1bd221e8fff0ffe9a6c8ef67e40da9df
wojtekrat <wojciech.ratajczak92@gmail.com>
2023-03-18 23:09:51 +0100

    ts

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,14 +5,14 @@ import '../styles/globals.css'
 // eslint-disable-next-line react/prop-types
 export default function RootLayout ({ children }) {
   return (
-    <html lang="en" className="bg-gradient-to-r from-zinc-900 via-stone-900 to-zinc-900 text-white no-select">
+    <html lang="en">
         <head>
           <title>Wojciech Ratajczak</title>
           <link rel="icon" type="image/png" sizes="32x32" href="/logo.png"></link>
           <link href="https://fonts.googleapis.com/css2?family=Tilt+Warp&display=swap" rel="stylesheet"></link>
           <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet"></link>
         </head>
-        <body>
+        <body className="bg-gradient-to-r from-zinc-900 via-stone-900 to-zinc-900 text-white no-select">
           <header>
             <Navbar />
           </header>
